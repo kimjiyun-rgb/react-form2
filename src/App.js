5dcdbfcb7c67839cf9192e5523427e5a1fa091bc
kimjiyun <faren02134@gmail.com>
2024-02-01 19:34:11 +0900

    87979797979

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import TextAreaForm from "./TextAreaForm";
+import SelectForm from "./SelectForm";
+function NameForm(props)
+{
+  const [value, setValue] = useState('');
+  const handleChange = (event) =>
+  {
+    setValue(event.target.value.toUpperCase());
+    props.func(event.target.value.toUpperCase());
+  };
+  const handleSubmit = (event) =>
+  {
+    alert(`입력한 이름: ${value}`);
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        이름:
+        <input type="text" value={value} onChange={handleChange} />
+      </label>
+      <button type="submit">제출</button>
+    </form>
+  )
+}
+function App()
+{
+  const [name, setName] = useState('');
+  function inputName(value)
+  {
+    setName(value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NameForm func={inputName} />
+      <TextAreaForm />
+      <SelectForm />
+      <hr />
+      이름: {name}, 요청사항: ???, 과일: ???
+      <hr />
+      <Signup />
     </div>
   );
 }
+
+const Signup = () =>
+{
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState('male');
+  return (
+    <div>
+      <lable htmlfor='name'>이름: </lable>
+      <input type='text' id='name' onChange={(e) =>
+      {
+        setName(e.target.value);
+      }} value={name} />
+
+      <label htmlFor='gender'>성별: </label>
+      <select id='gender' value={gender} onChange={(e) =>
+      {
+        const idx = e.target.selectedIndex;
+        console.log(e);
+        const value = e.target[idx].value;
+        setGender(value);
+      }}>
+        <option value='male'>남자</option>
+        <option value='female'>여자</option>
+      </select>
+
+      <button onClick={() =>
+      {
+        alert(`${name}, ${gender}`);
+      }}>제출</button>
+    </div>
+  );
+};
 
 export default App;
